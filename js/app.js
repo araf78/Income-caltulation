562db69427a7e3c75872397baef9b46854d19305
unknown <alarafath71@gmail.com>
2022-02-17 22:08:22 +0600

    saving part error message part

--- js/app.js
+++ js/app.js
@@ -22,59 +22,42 @@ function balance(){
     console.log(totalIncome)
     const totalExpenses = calcTotalExpenses();
     // balance calculation
-    if(isNaN(totalIncome)){
+    if(isNaN(totalIncome) || totalIncome < 0){
         // document.getElementById('error-message').style.display = 'block';
-        // totalIncome = '';
-       alert('⚠️ Give a number in Income  !');
+       alert('⚠️ Give a Positve number in Income  !');
     }
-    else if(isNaN(foodInput)){
-        alert('⚠️ Give a number in Food  !');
+    else if(isNaN(foodInput) || foodInput < 0){
+        alert('⚠️ Give a Positve number in Food  !');
     }
-    else if(isNaN(rentInput)){
-        alert('⚠️ Give a number in Rent  !');
+    else if(isNaN(rentInput) || rentInput < 0){
+        alert('⚠️ Give a Positve number in Rent  !');
     }
-    else if(isNaN(clothesInput)){
-        alert('⚠️ Give a number in Clothes  !');
+    else if(isNaN(clothesInput) || clothesInput < 0){
+        alert('⚠️ Give a Positve number in Clothes  !');
     }
     else{
     const balance = totalIncome - totalExpenses;
     console.log('balance', balance);
     document.getElementById('have-balance').innerText = balance;
     console.log(document.getElementById('have-balance').innerText,'balance')
-    return  document.getElementById('have-balance').innerText;
+    return  balance;
     }
 };
-/* // saving amount & remaning balance calculation
-document.getElementById('save-btn').addEventListener('click', function(){
-    const totalIncome = document.getElementById('total-income').value;
-    const saveMoneyInput = document.getElementById('save-money').value;
-    console.log(saveMoneyInput);
 
-    const savingAmount = (totalIncome * saveMoneyInput) / 100;
-    document.getElementById('saving-amount').innerText = savingAmount;
-    // console.log(document.getElementById('saving-amount').innerText);
-
-    const totalExpenses = calcTotalExpenses();
-    document.getElementById('remaining-balance').innerText = totalExpenses - savingAmount;
-    // console.log(document.getElementById('remaining-balance').innerText);
-}); */
-/* document.getElementById('input-pad').addEventListener('click',function(event){
-    const inputNumber = event.target.value; */
-    // const errorMessage = document.getElementById('error-message');
-       // saving amount & remaning balance calculation
-// document.getElementById('save-btn').addEventListener('click',..................
  function saveCalc(){
     const totalIncome = document.getElementById('total-income').value;
     const saveMoneyInput = document.getElementById('save-money').value;
-    // console.log(saveMoneyInput);
-  
+    if(isNaN(saveMoneyInput)){
+        alert('⚠️! Give a number !⚠️')
+    }
+    else if(saveMoneyInput < 0){
+        alert('⚠️! Give a Positive number.Okay !⚠️')
+    }
+    else{
     const savingAmount = (totalIncome * saveMoneyInput) / 100;
     document.getElementById('saving-amount').innerText = savingAmount;
-    // console.log(document.getElementById('saving-amount').innerText);
 
     const newBalance = balance();
     document.getElementById('remaining-balance').innerText = newBalance - savingAmount;
-    // console.log(document.getElementById('remaining-balance').innerText);
+    }
     };
-   
-// });
